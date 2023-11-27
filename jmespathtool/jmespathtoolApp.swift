09cf2c4652a1992597b185bb3c876c0d0511bc47
Norm Barnard <norm@normbarnard.com>
2023-11-27 18:51:39 -0500

    Initial Commit

---
+++ jmespathtool/jmespathtoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  jmespathtoolApp.swift
+//  jmespathtool
+//
+//  Created by Norman Barnard on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct jmespathtoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
