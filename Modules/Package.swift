4d8ffd25ece0e997a641f314b444a8164b43121b
Norm Barnard <norm@normbarnard.com>
2023-11-29 20:01:35 -0500

    fixed Module path
    - was in trash
    - added .gitignore

---
+++ Modules/Package.swift
@@ -0,0 +1,47 @@
+// swift-tools-version:5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+// MARK: - Dependency Convenience
+extension Target.Dependency {
+    // MARK: Internal
+
+    // MARK: 3rd Party
+
+    static var jmespath: Self {
+        .product(name: "JMESPath", package: "jmespath.swift")
+    }
+
+    static var dependencies: Self {
+        .product(name: "Dependencies", package: "swift-dependencies")
+    }
+}
+
+// MARK: - Package Definition
+let package = Package(
+    name: "Modules",
+    platforms: [.macOS(.v14)],
+    products: [
+        // Helpers
+        .library(name: "JMESPathClient", targets: ["JMESPathClient"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/adam-fowler/jmespath.swift", from: "1.0.2"),
+
+        // Pointfree
+        .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.0.0"),
+
+        // Test Dependencies
+    ],
+    targets: [
+        // Internal Helpers
+        .target(
+            name: "JMESPathClient",
+            dependencies: [
+                .jmespath,
+                .dependencies,
+            ]
+        ),
+    ]
+)
---
+++ Modules/Sources/JMESPathClient/JMESPathClient.swift
@@ -0,0 +1,38 @@
+import Dependencies
+import Foundation
+import JMESPath
+
+public struct JMESPathClient {
+    public let query: (String, String) throws -> String?
+    public init(
+        query: @escaping (String, String) throws -> String?
+    ) {
+        self.query = query
+    }
+}
+
+extension JMESPathClient: DependencyKey {
+    public static var liveValue = JMESPathClient(
+        query: { queryString, jsonInput in
+            let expression = try JMESExpression.compile(queryString)
+            let result = try expression.search(json: jsonInput, as: String.self)
+            return result
+        }
+    )
+}
+
+extension JMESPathClient: TestDependencyKey {
+    public static var testValue = JMESPathClient(
+        query: { _, _ in unimplemented("JMESPathClient::query", placeholder: nil) }
+    )
+    public static var previewValue = JMESPathClient(
+        query: { _, _ in nil }
+    )
+}
+
+public extension DependencyValues {
+    var jmesPathClient: JMESPathClient {
+        get { self[JMESPathClient.self] }
+        set { self[JMESPathClient.self] = newValue }
+    }
+}
